b973eb8c221b3d7b48495f66ef9d0ea5dc2e760d
kunthet <kunthet@gmail.com>
2024-05-19 17:34:00 +0700

    udpate requirements

--- google-vm-install.sh
+++ google-vm-install.sh
@@ -1,4 +1,32 @@
 
 # Ubuntu 20.04
-sudo apt-get update
-sudo apt-get install python3-pip
+
+folder=binance1
+pyenv=~/venv/binance
+
+sudo apt-get update --yes
+
+# install Git
+sudo apt-get install git-all --yes
+
+
+# Python venv
+sudo apt install python3-venv --yes
+python3 -m venv $pyenv
+
+# install PIP
+#sudo apt-get install python3-pip --yes
+
+
+folder=binance1
+cd ~
+mkdir ~/$folder
+cd ~/$folder
+
+# Clone repo
+git clone https://github.com/kunthet/binance-trade-bot.git
+cd binance-trade-bot
+$pyenv/bin/pip3 install -r requirements.txt
+echo "nohup $pyenv/bin/python3 -u -m binance_trade_bot &" > run.sh
+chmod +x run.sh
+chmod +x ./scripts/*.*
